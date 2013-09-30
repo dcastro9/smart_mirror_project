ff1405c5df311483732b9f5ca565bed38cc354a0
Daniel Castro <dcastro9@gatech.edu>
2013-09-30 12:08:11 -0400

    Basic video viewer established.

--- script.py
+++ script.py
@@ -3,3 +3,11 @@ import numpy
 import cv2
 
 video = cv2.VideoCapture("horse.avi")
+cv2.namedWindow("Test Video")
+while(True):
+    f, img = video.read()
+    if img != None:
+        cv2.imshow("Test Video", img)
+        cv2.waitKey(200) # Changes to this alter the frame rate.
+    else:
+        break
