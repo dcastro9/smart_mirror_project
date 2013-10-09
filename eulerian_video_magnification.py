60eb0de58b06638c354380bc5623b734d01ea6a6
root <root@evm-desktop.(none)>
2013-10-09 13:04:51 -0400

    First attempt at writing reduce. Basically using homework for reference. Have written notes in comments.

--- eulerian_video_magnification.py
+++ eulerian_video_magnification.py
@@ -28,3 +28,26 @@ class EulerianVideoMagnification(object):
                 break
         self._frames = numpy.array(self._frames)
 
+    def __reduce(_frames):
+        '''Reduces each frame of the video to half its size.
+
+        Do I need to create a new numpy array for each reduce or would I overwrite the current
+        frames array? I'm not sure if we'd need access to non-reduced ones.
+
+        Also, this is basically just from the HW5 assignment from summer. It contains a
+        generating_kernel method, which was defined above it (I'll put it below this function
+        for reference). Do we need this/what does it do?
+        '''
+        for frame in self._frames:
+            out = None
+            k = generating_kernel(0.4)
+            out = scipy.signal.convolve2d(frame,k,'same')
+            newFrame = out[::2,::2] #can we just overwrite frame?
+
+    def __generating_kernel(a):
+        '''Returns a 5x5 generating kernel with parameter a.
+
+        From HW5 Barcelona.
+        '''
+        w_1d = numpy.array([0.25 - a/2.0, 0.25, a, 0.25, 0.25 - a/2.0])
+        return numpy.outer(w_1d, w_1d)
