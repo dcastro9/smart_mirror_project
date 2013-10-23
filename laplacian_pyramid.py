cdd0bf95272ed48c730729cc1a4d610f65962f44
Daniel Castro <dcastro9@gatech.edu>
2013-10-23 13:30:06 -0400

    Pushing working version.

--- laplacian_pyramid.py
+++ laplacian_pyramid.py
@@ -1,3 +1,7 @@
+import logging
+import numpy as np
+
+from scipy.signal import convolve2d
 
 class LaplacianPyramid(object):
     """Represents the difference of gaussian pyramid for an image frame.
@@ -9,21 +13,46 @@ class LaplacianPyramid(object):
 
     def __init__(self, frame, levels):
         self._pyramid = []
-        self._pyramid.append(frame)
+        reduction_pyramid = []
+        expansion_pyramid = []
+        frame = frame[:,:,0]
+
+        reduction_pyramid.append(frame)
 
         # Create the kernel outside the loop, so you only do this once.
-        kernel = __generating_kernel(0.4)
+        kernel = self.generating_kernel(0.4)
 
-        # The idea here is to do the reduction and expansion automatically
-        # so we have a pyramid already generated, right now I'm just doing the
-        # reduction, but we can add the expansion right after in the loop.
+        # Reduction
         for x in range(levels):
-            out = scipy.signal.convolve2d(frame, kernel, 'same')
+            out = convolve2d(frame, kernel, 'same')
             frame = out[::2,::2]
-            self._pyramid.append(frame)
-    
-    def __generating_kernel(a):
+            reduction_pyramid.append(frame)
+
+        # Expansion
+        for level in range(1, len(reduction_pyramid)):
+            current_frame = reduction_pyramid[level]
+            out = np.zeros((current_frame.shape[0]*2, current_frame.shape[1]*2))
+            out[::2,::2] = current_frame
+            # 4 times because image would be 4 times weaker otherwise w/ conv.
+            expansion_pyramid.append(4*convolve2d(out, kernel, 'same'))
+
+        # Oh Python.
+        del reduction_pyramid[-1]
+
+        # Subtract the pyramids.
+        if (len(reduction_pyramid) != len(expansion_pyramid)):
+            logging.critical("Pyramid size does not match - cannot continue.")
+        else:
+            for level in range(len(reduction_pyramid)):
+                reduction = reduction_pyramid[level]
+                expansion = expansion_pyramid[level]
+                laplacian = reduction - expansion[0:reduction.shape[0], 
+                                                  0:reduction.shape[1]]
+                self._pyramid.append(laplacian)
+
+
+    def generating_kernel(self, a):
         '''Returns a 5x5 generating kernel based on parameter a.
         '''
-        w_1d = numpy.array([0.25 - a/2.0, 0.25, a, 0.25, 0.25 - a/2.0])
-        return numpy.outer(w_1d, w_1d)
+        w_1d = np.array([0.25 - a/2.0, 0.25, a, 0.25, 0.25 - a/2.0])
+        return np.outer(w_1d, w_1d)
--- script.py
+++ script.py
@@ -1,4 +1,10 @@
+import scipy
+
 from eulerian_video_magnification import EulerianVideoMagnification
+from laplacian_pyramid import LaplacianPyramid
 
 evm = EulerianVideoMagnification("sample.mp4")
-evm._frames # Has the frames in a numpy array.
+print "Done processing video."
+laplacian = LaplacianPyramid(evm._frames[0], 5)
+for level in range(len(laplacian._pyramid)):
+    scipy.misc.imsave('out' + str(level) + '.jpg', laplacian._pyramid[level])
